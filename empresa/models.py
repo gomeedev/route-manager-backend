29146fa6f10c94deddf7f3f10a06438c12a35c0b
Johann <johanns.gomeze@gmail.com>
2025-11-02 15:54:29 -0500

    feat(company): Implementar nombre para la BD desde los modelos

--- empresa/models.py
+++ empresa/models.py
@@ -4,9 +4,14 @@ from django.db import models
 
 # Create your models here.
 class Empresa(models.Model):
-    id_empresa = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    nit = models.CharField(max_length=30)
+    id_empresa = models.AutoField(primary_key=True)
+    nit = models.CharField(max_length=30, unique=True)
     nombre_empresa = models.CharField(max_length=50)
     telefono_empresa = models.CharField(max_length=30)
     
+    def __str__(self):
+        return self.nombre_empresa
+    
+    class Meta:
+        db_table = "empresa"
     
