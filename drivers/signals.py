ac39d38e1825ea6a54bd41625ad76b06b53186e9
Johann <johanns.gomeze@gmail.com>
2025-11-08 23:54:25 -0500

    fix(drivers): Corregir dependencia del estado_conducto cuando un usuario es admin

--- drivers/signals.py
+++ drivers/signals.py
@@ -1,13 +1,34 @@
 # drivers/signals.py
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.db import transaction
 from users.models import Usuario
 from .models import Driver
+
+
+user_roles = {"driver"}
+
+
+def is_driver_role(usuario):
+    if not usuario.rol:
+        return False
+    return usuario.rol.nombre_rol.strip().lower() in user_roles
+
 
 @receiver(post_save, sender=Usuario)
 def crear_estado_conductor(sender, instance, created, **kwargs):
     
-    if created: #Solo cuando se crea
-        if instance.rol and instance.rol.nombre_rol.lower() == "driver":
-            if not hasattr(instance, "estado_operativo"):
-                Driver.objects.create(conductor=instance, estado=Driver.status_driver.DISPONIBLE)
+    with transaction.atomic():
+        try:
+            driver = Driver.objects.select_for_update().get(conductor=instance)
+        except Driver.DoesNotExist:
+            driver = None
+            
+        # Si el usuario tiene rol de conductor
+        if is_driver_role(instance):
+            
+            if not driver:
+                Driver.objects.create(conductor=instance, estado_driver=Driver.status_driver.DISPONIBLE)
+        else:
+            if driver:
+                driver.delete()
