fa193370b2e5e02ee0cb699515c5b44f7b86ed90
Johann <johanns.gomeze@gmail.com>
2025-11-20 19:36:52 -0500

    feat(routes): Implementar endpoints de rutas

--- drivers/urls.py
+++ drivers/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import DriverListView, DriverDetailView, DriverStateUpdateView
+from .views import DriverListView, DriverDetailView
 
 
 urlpatterns = [
     path("", DriverListView.as_view(), name="drivers-list"),
     path("<int:id_conductor>/", DriverDetailView.as_view(), name="drivers-datails"),
-    path("<int:id_conductor>/estado/", DriverStateUpdateView.as_view(), name="drivers-update-estado"),
     
 ]
--- routes/admin.py
+++ routes/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Ruta, EntregaPaquete
+
 
 # Register your models here.
+admin.site.register(Ruta)
+admin.site.register(EntregaPaquete)
